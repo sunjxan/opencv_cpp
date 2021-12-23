b195d02cc38d28a7c0041d33b1301f15e9519753
Jian Sun <sunjxan@gmail.com>
2021-12-23 14:04:21 +0800

    Update imshow.cpp

--- imshow.cpp
+++ imshow.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <string>
 #include <opencv2/highgui/highgui.hpp>
 
@@ -7,13 +8,19 @@ int main()
 {
     string filePath = "../../opencv/samples/data/lena.jpg";
     string windowName = "lena.jpg";
-    cv::Mat img = cv::imread(filePath, -1);
+    
+    cv::Mat img = cv::imread(filePath, cv::IMREAD_UNCHANGED);
     if (img.empty()) {
         return -1;
     }
+    
+    int w = image.cols, h = image.rows, d = image.channels();
+    printf("width=%d, height=%d, depth=%d\n", w, h, d);
+    
     cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
     cv::imshow(windowName, img);
     cv::waitKey();
     cv::destroyWindow(windowName);
+    
     return 0;
 }
