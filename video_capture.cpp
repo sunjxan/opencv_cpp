bc31b90f4fa9e617634d9c1c7740c2cbc71ff83d
Jian Sun <sunjxan@gmail.com>
2021-12-23 14:18:49 +0800

    Update video_capture.cpp

--- video_capture.cpp
+++ video_capture.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <string>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -6,7 +7,8 @@ using namespace std;
 
 int main()
 {
-    string windowNamePrefix = "Video Capture";
+    string windowName = "Video Capture";
+    bool flag = true;
     cv::VideoCapture cap;
     cap.open(0);
     cv::Mat frame;
@@ -15,8 +17,15 @@ int main()
         if (frame.empty()) {
             break;
         }
-        cv::imshow(windowNamePrefix + "", frame);
+        if (flag) {
+            flag = false;
+            char str[100];
+            sprintf(str, " (%d, %d)", frame.cols, frame.rows);
+            windowName += string(str);
+        }
+        cv::imshow(windowName, frame);
         if (cv::waitKey(33) >= 0) {
+            cv::destroyWindow(windowName);
             break;
         }
     }
